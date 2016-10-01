4d901931364100342ecc689676cebcbf8daa1e3f
Yosua1011 <yosua1011@gmail.com>
2016-10-01 16:25:11 +0700

    Add delay 10 seconds Add timestamp program started

--- import_webbrowser.py
+++ import_webbrowser.py
@@ -1,3 +1,11 @@
+import time
 import webbrowser
 
-webbrowser.open("https://www.youtube.com/watch?v=ZcTMLkGZ0iI")
+total_breaks = 3
+break_count = 0
+
+print("This program started on"+time.ctime())
+while(break_count < total_breaks):
+	time.sleep(10)
+	webbrowser.open("https://www.youtube.com/watch?v=ZcTMLkGZ0iI")
+	break_count = break_count + 1
